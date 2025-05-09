f0d85f6412376c6203087c6c4826c159d75ff832
Alex Cerveny <blaziken4567@gmail.com>
2025-05-09 16:04:00 -0500

    Add files via upload

---
+++ Week06Project/App.java
@@ -0,0 +1,21 @@
+package Week06Project;
+
+
+public class App{
+	public static void main(String args[]){
+		Deck deck = new Deck();
+		System.out.println("New Deck has Been created!");
+		System.out.println("--------------");
+	}
+}
+ class Card{
+
+}
+
+class Deck{
+
+}
+
+class Players{
+	
+}
